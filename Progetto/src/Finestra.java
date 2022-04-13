cb14f97cab7e08fa1d06288b5f37eaba8ba16a13
alesalaa <ale.salamone10@gmail.com>
2022-04-13 16:56:47 +0200

    Gioco base finito
    Funzionalità base del gioco funzionanti.

--- Progetto/src/Finestra.java
+++ Progetto/src/Finestra.java
@@ -21,17 +21,19 @@ public class Finestra extends JPanel implements ActionListener, KeyListener {
 	private int ballY = 630;
 	private int ballXdir = 1;
 	private int ballYdir = -2;
+	private int score = 0;
 	
 	public Finestra() {
 		map = new Map(5, 5);
 		addKeyListener(this);
-		setFocusable(true);
 		setFocusTraversalKeysEnabled(false);
+		setFocusable(true);
 		timer = new Timer(10, this);
 		timer.start();
 	}
 	
 	public void paint(Graphics g) {
+		
 		g.setColor(Color.black);
 		g.fillRect(0, 0, 800, 700);
 		
@@ -44,22 +46,64 @@ public class Finestra extends JPanel implements ActionListener, KeyListener {
 		//Ball
 		g.setColor(Color.yellow);
 		g.fillOval(ballX, ballY, 20, 20);
+		
+		g.dispose();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
+		if(ballY > 700) {
+			play = false;
+			System.out.println("PERSO");
+		}
+		
 		if(play) {
-			ballX += ballXdir;
-			ballY += ballYdir;
 			
 			if(new Rectangle(paddleX, 650, 100, 12).intersects(new Rectangle(ballX, ballY, 20, 20))) {
 				ballYdir = -ballYdir;
 			}
 			
+			for(int i=0;i<map.map.length;i++) {
+				for(int j=0;j<map.map[0].length;j++) {
+					
+					if(map.map[i][j] == 1) {
+						
+						//Coordinate palla e cubetti 
+						int brickWidth = map.brickWidth;
+						int brickHeight = map.brickHeight;
+						int brickX = j * brickWidth + 140;
+						int brickY = i * brickHeight + 50;
+						
+						//Creazione rettangoli palla e cubetti
+						Rectangle brick = new Rectangle(brickX, brickY, brickWidth, brickHeight);
+						Rectangle ball = new Rectangle(ballX, ballY, 20, 20);
+						 
+						if(brick.intersects(ball)) {
+							score += 1;
+							System.out.println(score);
+							map.setBrick(i, j, 0);
+							
+							if(ballX+19 <= brick.x || ballX+2 >= brick.x+brick.width) {
+								ballXdir = -ballXdir;
+							}
+							else {
+								ballYdir = -ballYdir;
+							}
+							
+							break;
+						}
+					}
+				}
+			}
+			
+			ballX += ballXdir;
+			ballY += ballYdir;
+			
 			if(ballX < 0) {
 				ballXdir = -ballXdir;
 			}
-			if(ballX > 780) {
+			if(ballX > 765) {
 				ballXdir = -ballXdir;
 			}
 			if(ballY < 0) {
--- Progetto/src/Map.java
+++ Progetto/src/Map.java
@@ -4,9 +4,9 @@ import java.awt.Graphics2D;
 
 public class Map {
 	
-	private int map[][];
-	private int brickWidth;
-	private int brickHeight;
+	public int map[][];
+	public int brickWidth;
+	public int brickHeight;
 	
 	public Map(int col, int row) {
 		this.map = new int[row][col];
@@ -22,17 +22,24 @@ public class Map {
 	
 	public void draw(Graphics2D g) {
 		for(int i=0;i<map.length;i++) {
+			
 			for(int j=0;j<map[i].length;j++) {
-				
-				//Griglia
-				g.setColor(Color.blue);
-				g.fillRect(j*brickWidth+140, i*brickHeight+50, brickWidth, brickHeight);
-				
-				//Bordo griglia
-				g.setStroke(new BasicStroke(3));
-				g.setColor(Color.black);
-				g.drawRect(j * brickWidth+140, i * brickHeight+50, brickWidth, brickHeight);
+			
+				if(map[i][j] == 1) {
+					//Griglia
+					g.setColor(Color.blue);
+					g.fillRect(j*brickWidth+140, i*brickHeight+50, brickWidth, brickHeight);
+					
+					//Bordo griglia
+					g.setStroke(new BasicStroke(3));
+					g.setColor(Color.black);
+					g.drawRect(j * brickWidth+140, i * brickHeight+50, brickWidth, brickHeight);
+				}
 			}
 		}
+	}
+	
+	public void setBrick(int row, int col, int value) {
+		map[row][col] = value;
 	}
 }
